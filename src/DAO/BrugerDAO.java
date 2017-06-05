06853de743c677f53ce9e7e90a8a45fdb8d9cc27
Stuart <Stuart@DESKTOP-TI1TDQB.eduroam.wireless.dtu.dk>
2017-06-05 09:56:37 +0200

    df

--- src/DAO/BrugerDAO.java
+++ src/DAO/BrugerDAO.java
@@ -10,25 +10,29 @@ public class BrugerDAO implements IBrugerDAO
 {
 
 	@Override
-	public BrugerDTO getBruger(int oprId) throws DALException {
+	public BrugerDTO getBruger(int oprId) throws DALException 
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List<BrugerDTO> getBrugerList() throws DALException {
+	public List<BrugerDTO> getBrugerList() throws DALException 
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void createBruger(BrugerDTO opr) throws DALException {
+	public void createBruger(BrugerDTO opr) throws DALException 
+	{
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void updateBruger(BrugerDTO opr) throws DALException {
+	public void updateBruger(BrugerDTO opr) throws DALException 
+	{
 		// TODO Auto-generated method stub
 		
 	}
