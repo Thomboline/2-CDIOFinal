6550995668da6f84944ce2d708fc67bd54460196
Stuart <Stuart@DESKTOP-TI1TDQB.eduroam.wireless.dtu.dk>
2017-06-05 10:05:22 +0200

    d

--- src/DAO/BrugerDAO.java
+++ src/DAO/BrugerDAO.java
@@ -2,8 +2,10 @@ package DAO;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import Connector.Connector;
 import DAOInterfaces.DALException;
 import DAOInterfaces.IBrugerDAO;
 import DTO.BrugerDTO;
@@ -26,22 +28,39 @@ public class BrugerDAO implements IBrugerDAO
 	@Override
 	public List<BrugerDTO> getBrugerList() throws DALException 
 	{
-		// TODO Auto-generated method stub
-		return null;
+		List<OperatoerDTO> list = new ArrayList<OperatoerDTO>();
+		ResultSet rs = Connector.doQuery("SELECT * FROM operatoer");
+		try
+		{
+			while (rs.next()) 
+			{
+				list.add(new OperatoerDTO(rs.getInt("opr_id"), rs.getString("opr_navn"), rs.getString("ini"), rs.getString("cpr"), rs.getString("password")));
+			}
+		}
+		catch (SQLException e) { throw new DALException(e); }
+		return list;
+	}
 	}
 
 	@Override
 	public void createBruger(BrugerDTO opr) throws DALException 
 	{
-		// TODO Auto-generated method stub
-		
+		Connector.doUpdate
+		(
+				"INSERT INTO operatoer(opr_id, opr_navn, ini, cpr, password) VALUES " +
+				"(" + opr.getOprId() + ", '" + opr.getOprNavn() + "', '" + opr.getIni() + "', '" + 
+				opr.getCpr() + "', '" + opr.getPassword() + "')"
+		);
 	}
 
 	@Override
 	public void updateBruger(BrugerDTO opr) throws DALException 
 	{
-		// TODO Auto-generated method stub
-		
+		Connector.doUpdate(
+				"UPDATE operatoer SET  opr_navn = '" + opr.getOprNavn() + "', ini =  '" + opr.getIni() + 
+				"', cpr = '" + opr.getCpr() + "', password = '" + opr.getPassword() + "' WHERE opr_id = " +
+				opr.getOprId()
+		);
 	}
 	
 
