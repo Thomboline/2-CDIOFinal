e5baa50c963ed120b4742c63deebdbefb8ae1ef7
bijannegari <bijannegari@10.149.247.26>
2017-06-05 10:00:32 +0200

    Connector done madafak!

--- src/Connector/Connector.java
+++ src/Connector/Connector.java
@@ -1,4 +1,5 @@
-package connector01917;
+package Connector;
+
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -6,67 +7,38 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import daointerfaces01917.DALException;
 
+public class Connector {
+    private final String HOST     = "sql11.freemysqlhosting.net";
+    private final int    PORT     = 3306;
+    private final String DATABASE = "sql11178303";
+    private final String USERNAME = "sql11178303"; 
+    private final String PASSWORD = "CDIOfinal2017";
+    private Connection connection;
+    
+    public Connector() {
+        try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE;
+			connection = DriverManager.getConnection(url, USERNAME, PASSWORD);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+    }
 
-public class Connector
-{
-	/**
-	 * To connect to a MySQL-server
-	 * 
-	 * @param url must have the form
-	 * "jdbc:mysql://<server>/<database>" for default port (3306)
-	 * OR
-	 * "jdbc:mysql://<server>:<port>/<database>" for specific port
-	 * more formally "jdbc:subprotocol:subname"
-	 * 
-	 * @throws ClassNotFoundException 
-	 * @throws IllegalAccessException 
-	 * @throws InstantiationException 
-	 * @throws SQLException 
-	 */
-	public static Connection connectToDatabase(String url, String username, String password)
-			throws InstantiationException, IllegalAccessException,
-					ClassNotFoundException, SQLException
-	{
-		// call the driver class' no argument constructor
-		Class.forName("com.mysql.jdbc.Driver").newInstance();
-		
-		// get Connection-object via DriverManager
-		return (Connection) DriverManager.getConnection(url, username, password);
-	}
-	
-	private static Connection conn;
-	private static Statement stm;
-	
-	public Connector(String server, int port, String database,
-			String username, String password)
-				throws InstantiationException, IllegalAccessException,
-					ClassNotFoundException, SQLException
-	{
-		conn	= connectToDatabase("jdbc:mysql://"+server+":"+port+"/"+database,
-					username, password);
-		stm		= conn.createStatement();
-	}
-	
-	public Connector() throws InstantiationException, IllegalAccessException,
-			ClassNotFoundException, SQLException
-	{
-		this(Constant.server, Constant.port, Constant.database,
-				Constant.username, Constant.password);
-	}
-	
-	public static ResultSet doQuery(String cmd) throws DALException
-	{
-		try { return stm.executeQuery(cmd); }
-		catch (SQLException e) { throw new DALException(e); }
-	}
-	
-	public static int doUpdate(String cmd) throws DALException
-	{
-		try { return stm.executeUpdate(cmd); }
-		catch (SQLException e) { throw new DALException(e); }
-	}
-	
-	
+    public Connection getConnection(){
+    	return connection;
+    }
+    
+    public ResultSet doQuery(String query) throws SQLException{
+        Statement stmt = connection.createStatement();
+        ResultSet res = stmt.executeQuery(query);
+        return res;
+    }
+    
+    public void doUpdate(String query) throws SQLException{
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate(query);
+    }
 }
