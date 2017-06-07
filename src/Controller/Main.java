d0bc13ac941d858d5a0d25eadb161d58edd7cc9b
Stuart <Stuart@192.168.0.10>
2017-06-07 15:02:00 +0200

    tis

---
+++ src/Controller/Main.java
@@ -0,0 +1,21 @@
+package controller;
+
+import socket.SocketController;
+import weight.IWeightInterfaceController;
+import weight.gui.WeightInterfaceControllerGUI;
+import socket.ISocketController;
+/**
+ * Simple class to fire up application and inject implementations
+ * @author Christian
+ *
+ */
+public class Main {
+	private static boolean gui= true;
+
+	public static void main(String[] args) {
+		ISocketController socketHandler = new SocketController();
+		IWeightInterfaceController weightController = new WeightInterfaceControllerGUI();
+		IMainController mainCtrl = new WeightController(socketHandler, weightController);
+		mainCtrl.start();
+	}
+}
