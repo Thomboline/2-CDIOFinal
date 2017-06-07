9a7e6e3db33188182fcaae484cf5e0c9668c2e2c
Thomas-PC <Thomas-PC@192.168.0.4>
2017-06-07 14:51:26 +0200

    Html 1.4

--- WebContent/HTML/User/User.js
+++ WebContent/HTML/User/User.js
@@ -1,3 +1,39 @@
-/**
- * Created by jonathanlarsen on 07/06/2017.
- */
+$(document).ready(function() {
+	$('#ListUsers').submit(function(event) {
+		event.preventDefault();
+		ListUsers();
+		return false;
+	});
+	
+	$('#CreateUser').submit(function(event) {
+		event.preventDefault();
+		CreateUser();
+		return false;
+	});
+});
+
+function ListUsers() {
+	$.ajax({
+		type: 'GET',
+		url: 'http://localhost:8080/CDIOFinal',
+		dataType: 'json',
+		success: renderList
+	});
+}
+
+function CreateUser() {
+	$.ajax({
+		type: 'POST',
+		url: 'http://localhost:8080/CDIOFinal',
+		dataType: "json",
+		data: 
+	});
+}
+
+function renderList(data) {
+	var list = data == null ? [] : (data instanceof Array ? data : [data]);
+
+	$.each(list, function(index, user) {
+		$('#UserList').append('<li><a href="#" data-identity="' + user.id + '">'+user.name+'</a></li>');
+	});
+}
--- WebContent/HTML/js/Main.js
+++ WebContent/HTML/js/Main.js
@@ -1,61 +1,99 @@
-/**
- * Created by jonathanlarsen on 07/06/2017.
- */
-$(document).ready(function() {
-    $('#ListUsers').submit(function(event) {
-        event.preventDefault();
-        ListUsers();
-        return false;
-    });
-
-    $('#CreateUser').submit(function(event) {
-        event.preventDefault();
-        CreateUser();
-        return false;
-    });
+$(document).ready( function() {
+	$("#LU").on("click", function() {
+		$("#wrapper").load("User/ListUser.html");
+	});
+	
+	$("#CU").on("click", function() {
+		$("#wrapper").load("User/CreateUser.html");
+	});
+	
+	$("#EU").on("click", function() {
+		$("#wrapper").load("User/EditUser.html");
+	});
+	
+	$("#DU").on("click", function() {
+		$("#wrapper").load("User/DeleteUser.html");
+	});
+	
+	$("#LR").on("click", function() {
+		$("#wrapper").load("Raavare/ListRaavare.html");
+	});
+	
+	$("#CR").on("click", function() {
+		$("#wrapper").load("Raavare/CreateRaavare.html");
+	});
+	
+	$("#ER").on("click", function() {
+		$("#wrapper").load("Raavare/EditRaavare.html");
+	});
+	
+	$("#DR").on("click", function() {
+		$("#wrapper").load("Raavare/DeleteRaavare.html");
+	});	
+	
+	$("#LRB").on("click", function() {
+		$("#wrapper").load("RaavareBatch/ListRaavareBatch.html");
+	});
+	
+	$("#CRB").on("click", function() {
+		$("#wrapper").load("RaavareBatch/CreateRaavareBatch.html");
+	});
+	
+	$("#ERB").on("click", function() {
+		$("#wrapper").load("RaavareBatch/EditRaavareBatch.html");
+	});
+	
+	$("#DRB").on("click", function() {
+		$("#wrapper").load("RaavareBatch/DeleteRaavareBatch.html");
+	});
+	
+	$("#LPB").on("click", function() {
+		$("#wrapper").load("ProduktBatch/LisrProduktBatch.html");
+	});
+	
+	$("#CPB").on("click", function() {
+		$("#wrapper").load("ProduktBatch/CreateProduktBatch.html");
+	});
+	
+	$("#EPB").on("click", function() {
+		$("#wrapper").load("ProduktBatch/EditProduktBatch.html");
+	});
+	
+	$("#DPB").on("click", function() {
+		$("#wrapper").load("ProduktBatch/DeleteProduktBatch.html");
+	});
+	
+	$("#LRE").on("click", function() {
+		$("#wrapper").load("Recept/ListRecept.html");
+	});
+	
+	$("#CRE").on("click", function() {
+		$("#wrapper").load("Recept/CreateRecept.html");
+	});
+	
+	$("#ERE").on("click", function() {
+		$("#wrapper").load("Recept/EditRecept.html");
+	});
+	
+	$("#DRE").on("click", function() {
+		$("#wrapper").load("Recept/DeleteRecept.html");
+	});
 });
 
 function MenuBar() {
-    if(user.hasRole('Administrator')) {
-        $("div.Admin").show();
-    }
-
-    if(user.hasRole('Farmaceut')) {
-        $("div.Farmaceut").show();
-    }
-
-    if(user.hasRole('Vaerkfoerer')) {
-        $("div.Vaerkfoereren").show();
-    }
-
-    if(user.hasRole('Laborant')) {
-        $("div.Laborant").show();
-    }
+	if(user.hasRole('Administrator')) {
+		$("div.Admin").show();
+	}
+	
+	if(user.hasRole('Farmaceut')) {
+		$("div.Farmaceut").show();
+	}
+	
+	if(user.hasRole('Vaerkfoerer')) {
+		$("div.Vaerkfoereren").show();
+	}
+	
+	if(user.hasRole('Laborant')) {
+		$("div.Laborant").show();
+	}
 }
-
-function ListUsers() {
-    $.ajax({
-        type: 'GET',
-        url: 'http://localhost:8080/CDIOFinal/',
-        dataType: 'json',
-        success: renderList
-    });
-}
-
-function CreateUser() {
-    $.ajax({
-        type: 'POST',
-        url: 'http://localhost:8080/CDIOFinal/',
-        dataType: "json",
-        data:
-    });
-}
-
-function renderList(data) {
-    var list = data == null ? [] : (data instanceof Array ? data : [data]);
-
-    $.each(list, function(index, user) {
-        $('#UserList').append('<li><a href="#" data-identity="' + user.id + '">'+user.name+'</a></li>');
-    });
-}
-
