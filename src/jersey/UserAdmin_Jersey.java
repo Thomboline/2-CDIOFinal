a6344917a6474edfc7fbe8564f30fce052d3e8ea
Thomas-PC <Thomas-PC@192.168.0.4>
2017-06-08 16:05:42 +0200

    html 1.8

--- src/jersey/UserAdmin_Jersey.java
+++ src/jersey/UserAdmin_Jersey.java
@@ -26,7 +26,33 @@ public class UserAdmin_Jersey {
 	@GET
 	@Produces({MediaType.APPLICATION_JSON})
 	public String showMessage() {
-		return null;
+		return "it works!";
 	}
 	
+	@GET
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public List<IUserDTO> ListUsers() throws DALException {
+		System.out.println("findAll");
+		return dao.getUserList();
+	}
+	
+	@PUT @Path("{id}")
+	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public IUserDTO update(IUserDTO user) throws DALException 
+	{
+		System.out.println("Updating user: " + user.getUserName());
+		dao.updateUser(user, 1);
+		return user;
+	}
+	
+	@PUT @Path("{id}")
+	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public IUserDTO delete(IUserDTO user) throws DALException 
+	{
+		System.out.println("Updating user: " + user.getUserName());
+		dao.updateUser(user, 0);
+		return user;
+	}
 }
