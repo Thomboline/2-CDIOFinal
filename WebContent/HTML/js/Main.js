f9889714baad94f58b9da4446cb7fe4c28ea6f76
jonathanlarsen <jal@atol.dk>
2017-06-09 11:07:35 +0200

    HTML Log ud og profile settings

--- WebContent/HTML/js/Main.js
+++ WebContent/HTML/js/Main.js
@@ -1,88 +1,88 @@
 $(document).ready( function() {
+    $(".Farmaceut").load("Administration.html #RaavareAdmin, #RaavareBatchAdmin, #ProduktBatchAdmin, #ReceptAdmin", function () {
+        $("#LU").on("click", function() {
+            $("#wrapper").load("User/ListUser.html");
+        });
 
-	$("#LU").on("click", function() {
-		$("#wrapper").load("User/ListUser.html");
-	});
-	
-	$("#CU").on("click", function() {
-		$("#wrapper").load("User/CreateUser.html");
-	});
-	
-	$("#EU").on("click", function() {
-		$("#wrapper").load("User/EditUser.html");
-	});
-	
-	$("#DU").on("click", function() {
-		$("#wrapper").load("User/DeleteUser.html");
-	});
-	
-	$("#LR").on("click", function() {
-		$("#wrapper").load("Raavare/ListRaavare.html");
-	});
-	
-	$("#CR").on("click", function() {
-		$("#wrapper").load("Raavare/CreateRaavare.html");
-	});
-	
-	$("#ER").on("click", function() {
-		$("#wrapper").load("Raavare/EditRaavare.html");
-	});
-	
-	$("#DR").on("click", function() {
-		$("#wrapper").load("Raavare/DeleteRaavare.html");
-	});	
-	
-	$("#LRB").on("click", function() {
-		$("#wrapper").load("RaavareBatch/ListRaavareBatch.html");
-	});
-	
-	$("#CRB").on("click", function() {
-		$("#wrapper").load("RaavareBatch/CreateRaavareBatch.html");
-	});
-	
-	$("#ERB").on("click", function() {
-		$("#wrapper").load("RaavareBatch/EditRaavareBatch.html");
-	});
-	
-	$("#DRB").on("click", function() {
-		$("#wrapper").load("RaavareBatch/DeleteRaavareBatch.html");
-	});
-	
-	$("#LPB").on("click", function() {
-		$("#wrapper").load("ProduktBatch/LisrProduktBatch.html");
-	});
-	
-	$("#CPB").on("click", function() {
-		$("#wrapper").load("ProduktBatch/CreateProduktBatch.html");
-	});
-	
-	$("#EPB").on("click", function() {
-		$("#wrapper").load("ProduktBatch/EditProduktBatch.html");
-	});
-	
-	$("#DPB").on("click", function() {
-		$("#wrapper").load("ProduktBatch/DeleteProduktBatch.html");
-	});
-	
-	$("#LRE").on("click", function() {
-		$("#wrapper").load("Recept/ListRecept.html");
-	});
-	
-	$("#CRE").on("click", function() {
-		$("#wrapper").load("Recept/CreateRecept.html");
-	});
-	
-	$("#ERE").on("click", function() {
-		$("#wrapper").load("Recept/EditRecept.html");
-	});
-	
-	$("#DRE").on("click", function() {
-		$("#wrapper").load("Recept/DeleteRecept.html");
-	});
+        $("#CU").on("click", function() {
+            $("#wrapper").load("User/CreateUser.html");
+        });
 
-	$(function () {
-        $(".Farmaceut").load("Administration.html #RaavareAdmin, #RaavareBatchAdmin, #ProduktBatchAdmin, #ReceptAdmin");
-    })
+        $("#EU").on("click", function() {
+            $("#wrapper").load("User/EditUser.html");
+        });
+
+        $("#DU").on("click", function() {
+            $("#wrapper").load("User/DeleteUser.html");
+        });
+
+        $("#LR").on("click", function() {
+            $("#wrapper").load("Raavare/ListRaavare.html");
+        });
+
+        $("#CR").on("click", function() {
+            $("#wrapper").load("Raavare/CreateRaavare.html");
+        });
+
+        $("#ER").on("click", function() {
+            $("#wrapper").load("Raavare/EditRaavare.html");
+        });
+
+        $("#DR").on("click", function() {
+            $("#wrapper").load("Raavare/DeleteRaavare.html");
+        });
+
+        $("#LRB").on("click", function() {
+            $("#wrapper").load("RaavareBatch/ListRaavareBatch.html");
+        });
+
+        $("#CRB").on("click", function() {
+            $("#wrapper").load("RaavareBatch/CreateRaavareBatch.html");
+        });
+
+        $("#ERB").on("click", function() {
+            $("#wrapper").load("RaavareBatch/EditRaavareBatch.html");
+        });
+
+        $("#DRB").on("click", function() {
+            $("#wrapper").load("RaavareBatch/DeleteRaavareBatch.html");
+        });
+
+        $("#LPB").on("click", function() {
+            $("#wrapper").load("ProduktBatch/LisrProduktBatch.html");
+        });
+
+        $("#CPB").on("click", function() {
+            $("#wrapper").load("ProduktBatch/CreateProduktBatch.html");
+        });
+
+        $("#EPB").on("click", function() {
+            $("#wrapper").load("ProduktBatch/EditProduktBatch.html");
+        });
+
+        $("#DPB").on("click", function() {
+            $("#wrapper").load("ProduktBatch/DeleteProduktBatch.html");
+        });
+
+        $("#LRE").on("click", function() {
+            $("#wrapper").load("Recept/ListRecept.html");
+        });
+
+        $("#CRE").on("click", function() {
+            $("#wrapper").load("Recept/CreateRecept.html");
+        });
+
+        $("#ERE").on("click", function() {
+            $("#wrapper").load("Recept/EditRecept.html");
+        });
+
+        $("#DRE").on("click", function() {
+            $("#wrapper").load("Recept/DeleteRecept.html");
+        });
+    });
+
+
+
 
     $("#ProfilIndstillinger").on("click", function () {
         $("#wrapper").load("User/ProfileSettings.html")
