8baf104c71410caeb3e87e06a630e127af893e39
jonathanlarsen <jal@atol.dk>
2017-06-10 21:55:45 +0200

    small Interface changes

--- src/jersey/UserAdmin_Jersey.java
+++ src/jersey/UserAdmin_Jersey.java
@@ -7,23 +7,20 @@
  */
 package jersey;
 
-import java.util.ArrayList;
 import java.util.List;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import connector.DALException;
+import dao.BrugerDAO;
 import daointerfaces.IBrugerDAO;
 import dtointerfaces.IBrugerDTO;
+
 
 @Path("/UserService")
 public class UserAdmin_Jersey {
@@ -33,27 +30,27 @@ public class UserAdmin_Jersey {
 	public String showMessage() {
 		return "it works!";
 	}
+
+
 	
-	IBrugerDAO dao = new IBrugerDAO();
+	IBrugerDAO dao = new BrugerDAO();
 	
 	@GET
 	@Path("/users")
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-	public List <IBrugerDTO> ListUsers() throws DALException {
+	public List<IBrugerDTO> ListUsers() throws Exception {
 		
-		return dao.getUserList();
+		return dao.getBrugerList();
 	}
 	
 	@PUT
 	@Path("/users/{id}")
 	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-	public IBrugerDTO update(IBrugerDTO user) throws DALException {
+	public IBrugerDTO update(IBrugerDTO user) throws Exception {
 		System.out.println("Updating user: " + user.getBrugerNavn());
-		
-		return user;
 
-		dao.updateUser(user, 1);
+		dao.updateBruger(user);
 		return user;
 	}
 	
